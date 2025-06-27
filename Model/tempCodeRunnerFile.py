05b4c36588289d142adc6507a961893a7acdc618
Amitabh Chakravorty <amitabh822@gmail.com>
2025-06-26 22:00:59 -0400

    Update tempCodeRunnerFile.py
    Add SoftcbDiceLoss and integrate it into training script

--- Model/tempCodeRunnerFile.py
+++ Model/tempCodeRunnerFile.py
@@ -1,2 +1,9 @@
+from losses.cbdice_loss import SoftcbDiceLoss  
 
-model = UNetBiFPN(
+model = UNetBiFPN()
+
+criterion = SoftcbDiceLoss()
+
+# Example:
+# predictions = model(inputs)
+# loss = criterion(predictions, targets)
