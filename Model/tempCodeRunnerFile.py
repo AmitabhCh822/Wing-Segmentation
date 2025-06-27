1314b0ab910a96d70d70cffe4f866018f431a936
Amitabh Chakravorty <amitabh822@gmail.com>
2025-06-26 22:12:50 -0400

    Update tempCodeRunnerFile.py
    Update

--- Model/tempCodeRunnerFile.py
+++ Model/tempCodeRunnerFile.py
@@ -1,8 +1,11 @@
 import sys
 import os
+
+# Add the root directory of your project to Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from losses.cbdice_loss import SoftcbDiceLoss
+
 
 model = UNetBiFPN()
 
